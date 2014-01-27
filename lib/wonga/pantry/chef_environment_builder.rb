11fff9289a3c2b4f1065fd7e351d37ab714581d6
John Coleman <john.coleman@wonga.com>
2014-01-27 18:08:28 +0000

    TD-1756 Add CI INT RC STG WIP environment type support

--- lib/wonga/pantry/chef_environment_builder.rb
+++ lib/wonga/pantry/chef_environment_builder.rb
@@ -12,35 +12,46 @@ module Wonga
       end
 
       def build!(message)
+        # required for ERB
+        @message = message
+        @team_name = normalize_name(@message['team_name'])
+        @env_name = normalize_name(@message['environment_name'])
+        @env_type = normalize_name(@message['environment_type'])
+
         e = Chef::Environment.new
-        name = chef_environment(message)
+        name = chef_environment_name(@message)
         e.name(name)
+        e.description(@message['description'])
         @logger.info("Building environment #{e.name}")
 
-        @message = message # require for ERB
-        e.default_attributes = eval(ERB.new(File.read(File.join(@template_path, "jenkins_attributes.erb"))).result(binding))
+        e.default_attributes = eval(ERB.new(File.read(File.join(@template_path, "#{@env_type}_environment.erb"))).result(binding))
         e.create
         name
       end
 
-      private
-      def chef_environment(message)
-        environments = Chef::Environment.list.keys
-        team_name = message["team_name"].parameterize.gsub('_', '-').gsub('--', '-')
-        if !environments.include?(team_name)
-          team_name
+      def normalize_name(name)
+        name.parameterize.gsub('_', '-').gsub('--', '-')
+      end
+
+      def first_env(envs, name, i=1)
+        if !envs.include? "#{name}-#{i}"
+          "#{name}-#{i}"
         else
-          first_env(environments, team_name)
+          first_env(envs, name, i+1)
         end
       end
 
-      def first_env(envs, team, i=1)
-        if !envs.include? "#{team}-#{i}"
-          "#{team}-#{i}"
+      private
+      def chef_environment_name(message)
+        environments = Chef::Environment.list.keys
+        chef_environment_name = "#{@team_name}-#{@env_type}-#{@env_name}"
+        if !environments.include?(chef_environment_name)
+          chef_environment_name
         else
-          first_env(envs, team, i+1)
+          first_env(environments, chef_environment_name)
         end
       end
+
     end
   end
 end
